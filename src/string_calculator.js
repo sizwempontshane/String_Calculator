13cd3ec66b4efe6d48a03ac42357c6471c721eee
SizweSeize <mpontshanesizwe@gmail.com>
2020-05-29 17:53:27 +0200

    updated string calculator

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,69 +1,45 @@
-function add(str){
-    if (str === ""){
-      return 0
-    }
-    if (str[0] == "/" && str[1] == "/" && /.+\n/.test(str) == false) {
-        throw new Error("invalid input");
-      }
-      if (
-        /[0-9]$/g.test(str) == false &&
-        str.length > 0 &&
-        /\s/g.test(str) == true
-      ) {
-        throw new Error("invalid input");
-      }
-    const delimiter = getDelimiter(str)
-    const formattedInput = formatInput(str)
-    return calculateSum(getNumbers(formattedInput, delimiter)) 
+function add(str) {
+  let numbers = "";
+  if (str == "" || str == 1) {
+    return Number(str * 1);
   }
-  function formatInput(str){
-    const delimiterRegex = /^(\/\/.*\n)/
-    const matches = delimiterRegex.exec(str)
-    if(matches && matches.length > 0){
-      return str.replace(delimiterRegex,"")
-    }
-    return str
+  if (str[0] == "/" && str[1] == "/" && /\d/.test(str[str.length - 1])) {
+    numbers = getDelimiter(str);
+  } else if (
+    str.match(/[\n]/) ||
+    (/\d/.test(str[str.length - 1]) &&
+      /[-\d\d]/.test(str[0]) &&
+      str.match(/[\/]/g) == null)
+  ) {
+    numbers = str.split(/[\,\n]+/);
   }
-  function getDelimiter(str) {
-    const delimiters = []
-    const multipleDelimiterRegexp = /(?:^\/\/)?\[([^\[\]]+)\]\n?/g
-    let matches = multipleDelimiterRegexp.exec(str)
-    for (let i = 0; matches !== null; i++) {
-      delimiters.push(matches[1])
-     matches = multipleDelimiterRegexp.exec(str)  
-    }
-    if(delimiters.length > 0){
-      return new RegExp("["+delimiters.join("")+"]")
-    }
-    matches = (/^\/\/(.*?)\n/g).exec(str)
-    if(matches && matches[1]){
-      return matches[1]
-    }
-    return /[\n,]/
+  if (str.match(/.+(?=\/\/)/g) != null && /\n/.test(str) == true) {
+    throw new Error("invalid input");
   }
-  function getNumbers(string, delimiter){
-    return string.split(delimiter)
-      .filter(n => n !== "")
-      .map(n => parseInt(n))
+  if (str.match(/\d$/g) == null && str.length > 0) {
+    throw new Error("invalid input");
   }
-  function calculateSum(numbers){
-    const negatives = []
-    const finalSum = numbers.reduce((sum, n) =>{
-      if(n >= 1000){
-        return 0
-      }
-      if(n < 0){
-        negatives.push(n)
-        return 0
-      }
-      return sum + n
-    },0)
-    if(negatives.length > 0){
-      throw Error('negatives not allowed '+negatives.join(','))
-    }
-    return finalSum
+  if (str.toString().match(/-\d+/g)) {
+    throw Error("negatives not allowed " + str.toString().match(/-\d+/g));
   }
+  return numbers
+    .filter(function numbersLessThan1000(num) {
+      return num < 1000;
+    })
+    .reduce((sum, n) => parseInt(sum) + parseInt(n));
+}
+function getDelimiter(str) {
+  let delimiter = str.match(/\[(.*?)\]/g);
+  let expression = str.slice(str.search("\n") + 1, str.length);
+  if (delimiter == null) {
+    return expression.split(str.slice(2, str.search("\n")));
+  }
+  for (let i = 0; i < delimiter.length; i++) {
+    expression = expression.split(delimiter[i].slice(1, -1)).join(" ");
+  }
+  return expression.split(" ");
+}
 
-console.log(add("1,2,3//;\n1000,1;2"))
+console.log(add(""));
 
-module.exports = {add}
+module.exports = { add };
